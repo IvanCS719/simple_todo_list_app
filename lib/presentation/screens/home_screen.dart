3bae65160261c0962ed3ae1dbbf34efd35d3a84d
IvanCS719 <ivancupil17@gmail.com>
2025-09-04 11:21:51 -0600

    feat: implementa widget fieldbox y boton para agregar una nueva tarea

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -12,6 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
       appBar: AppBar(
         title: const Text('Mis Tareas'),
       ),
@@ -29,8 +30,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     check = !check;
                   });
                 },
-                title: Text('Tarea X'),
-                secondary: IconButton(icon: Icon(Icons.delete_outlined), onPressed: (){},),
+                title: const Text('Tarea X'),
+                secondary: IconButton(icon: const Icon(Icons.delete_outlined), onPressed: (){},),
                 controlAffinity: ListTileControlAffinity.leading,
                 );
               
@@ -50,8 +51,69 @@ class _HomeScreenState extends State<HomeScreen> {
               );*/
             }
             ),
+          ),
+
+          Container(
+            color: Theme.of(context).colorScheme.surfaceContainerLowest,
+            padding: const EdgeInsets.all(16.0),
+            child: const TaskInputBox()
+              
+              ,
           )
         ],
+      ),
+    );
+  }
+}
+
+class TaskInputBox extends StatelessWidget {
+  const TaskInputBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Expanded(child: const TaskFieldBox()),
+        const SizedBox(width: 8,),
+        IconButton.filled(
+          padding: const EdgeInsets.all(12.0),
+          onPressed: (){},
+          icon: const Icon(Icons.add_outlined)
+          )
+      ],
+      );
+  }
+}
+
+class TaskFieldBox extends StatelessWidget {
+  const TaskFieldBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textCotroller = TextEditingController();
+    final focusNodeV = FocusNode();
+
+    final outlineInputBorder = OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade400,
+            width: 1.5
+          ),
+          borderRadius: BorderRadius.circular(40)
+        );
+    
+    return TextFormField(
+      controller: textCotroller,
+      focusNode: focusNodeV,
+      decoration: InputDecoration(
+        hintText: 'Añadir una tarea nueva...',
+        filled: true,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder
       ),
     );
   }
