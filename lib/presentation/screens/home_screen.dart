096948bcb7ae57340f7c4a84340319ba3b9ac319
IvanCS719 <ivancupil17@gmail.com>
2025-09-05 13:44:03 -0600

    feat: implementar con provider en HomeScreen para gestión de tareas

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:simple_todo_list_app/presentation/providers/task_list_provider.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends ConsumerWidget {
+
+  final TextEditingController textController;
+  final FocusNode focusNodeV;
+  
+  const HomeScreen({super.key, required this.textController, required this.focusNodeV});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
 
-class _HomeScreenState extends State<HomeScreen> {
-  bool check = false;
-  @override
-  Widget build(BuildContext context) {
+    final tasks = ref.watch(taskListProvider);
+    
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
       appBar: AppBar(
@@ -20,46 +23,38 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index){
-              return CheckboxListTile(
-                value: check,
-
-                onChanged: (value){
-                  setState(() {
-                    check = !check;
-                  });
-                },
-                title: const Text('Tarea X'),
-                secondary: IconButton(icon: const Icon(Icons.delete_outlined), onPressed: (){},),
-                controlAffinity: ListTileControlAffinity.leading,
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                // Checkbox inmutable, por ser StatelessWidget
+                final task = tasks[index];
+                return CheckboxListTile(
+                  value: task.isDone,
+                  onChanged: (_){
+                    ref.read(taskListProvider.notifier).toggleTask(task.id);
+                  }, // No permite cambiar el checkbox
+                  title: Text(
+                    task.title,
+                    style: task.isDone ? TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      color: Colors.black.withOpacity(0.5),
+                    ) : null,
+                    ),
+                  secondary: IconButton(
+                    icon: const Icon(Icons.delete_outlined),
+                    onPressed: () {
+                      ref.read(taskListProvider.notifier).removeTask(task.id);
+                    },
+                  ),
+                  controlAffinity: ListTileControlAffinity.leading,
                 );
-              
-              /*ListTile(
-                leading: Checkbox(value: check, onChanged: (value){
-                  setState(() {
-                    check = !check;
-                  });
-                }),
-                title: Text('Tarea'),
-                trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.delete_outline)),
-                onTap: (){
-                  setState(() {
-                    check = !check;
-                  });
-                },
-              );*/
-            }
+              },
             ),
           ),
-
           Container(
             color: Theme.of(context).colorScheme.surfaceContainerLowest,
             padding: const EdgeInsets.all(16.0),
-            child: const TaskInputBox()
-              
-              ,
-          )
+            child: TaskInputBox(textController: textController, focusNodeV: focusNodeV, ref: ref,),
+          ),
         ],
       ),
     );
@@ -67,19 +62,38 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class TaskInputBox extends StatelessWidget {
+
+  final TextEditingController textController;
+  final FocusNode focusNodeV;
+
+  final WidgetRef ref;
+
   const TaskInputBox({
     super.key,
+    required this.textController,
+    required this.focusNodeV,
+    required this.ref
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Expanded(child: const TaskFieldBox()),
+        Expanded(child: TaskFieldBox(textController: textController, focusNodeV: focusNodeV,)),
         const SizedBox(width: 8,),
         IconButton.filled(
           padding: const EdgeInsets.all(12.0),
-          onPressed: (){},
+          onPressed: (){
+
+            final taskText = textController.text.trim();
+
+            if (taskText.isNotEmpty) {
+              focusNodeV.requestFocus();
+              ref.read(taskListProvider.notifier).addTask(taskText);
+              textController.clear();
+              
+            }
+          },
           icon: const Icon(Icons.add_outlined)
           )
       ],
@@ -88,14 +102,19 @@ class TaskInputBox extends StatelessWidget {
 }
 
 class TaskFieldBox extends StatelessWidget {
+
+  final TextEditingController textController;
+  final FocusNode focusNodeV;
+
   const TaskFieldBox({
     super.key,
+    required this.textController,
+    required this.focusNodeV
   });
 
   @override
   Widget build(BuildContext context) {
-    final textCotroller = TextEditingController();
-    final focusNodeV = FocusNode();
+    
 
     final outlineInputBorder = OutlineInputBorder(
           borderSide: BorderSide(
@@ -106,8 +125,9 @@ class TaskFieldBox extends StatelessWidget {
         );
     
     return TextFormField(
-      controller: textCotroller,
+      controller: textController,
       focusNode: focusNodeV,
+      //onTapOutside: (event) => focusNodeV.unfocus(),
       decoration: InputDecoration(
         hintText: 'Añadir una tarea nueva...',
         filled: true,
