dc772d628d455c6f52d329103c3c865932b24c28
IvanCS719 <ivancupil17@gmail.com>
2025-09-09 08:08:44 -0600

    feat: convertir MyApp en ConsumerWidget para detectar cambios de tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_todo_list_app/presentation/home_container.dart';
+import 'package:simple_todo_list_app/presentation/providers/theme_provider.dart';
 
 void main(){
   return runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final appTheme = ref.watch(themeNotifierProvider).themeApp();
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeContainer(),
+      theme: appTheme,
+      home: const HomeContainer(),
     );
   }
 }
